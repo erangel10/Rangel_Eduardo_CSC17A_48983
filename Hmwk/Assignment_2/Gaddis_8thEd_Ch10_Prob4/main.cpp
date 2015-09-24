ba87c64057a60d1587159664352591be0b8324ed
erangel10 <rangeleduardo10@gmail.com>
2015-09-24 09:51:41 -0700

    Signed-off-by: erangel10 <rangeleduardo10@gmail.com>

---
+++ Hmwk/Assignment_2/Gaddis_8thEd_Ch10_Prob4/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Eduardo Rangel
+ * Purpose:  
+ * Created on September 24, 2015, 8:51 AM
+ */
+//System libraries
+#include <iostream>
+
+using namespace std;
+//User Libraries 
+
+//Global Constants 
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    return 0;
+}
+
---
+++ Hmwk/Assignment_2/Gaddis_8thEd_Ch9_Prob6/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Eduardo Rangel
+ * Purpose: Modify program 9-19
+ * Created on September 24, 2015, 8:43 AM
+ */
+//System Libraries
+#include <iostream>
+
+using namespace std;
+//User Libraries
+
+//Global Constants 
+
+//Function Prototypes 
+void arrSort(int *[], int);
+void showAry(const int [], int);
+void showPtr(int *[], int);
+//Execution begins here
+int main(int argc, char** argv) {
+    //declare variables
+    const int numDntn = 15;
+    
+    
+
+    return 0;
+}
+
