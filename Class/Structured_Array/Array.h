220afab2d356a32c35c9563c24ad544ce2fefd67
erangel10 <rangeleduardo10@gmail.com>
2015-10-01 12:35:34 -0700

    Signed-off-by: erangel10 <rangeleduardo10@gmail.com>

---
+++ Class/Structured_Array/Array.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Array.h
+ * Author: rcc
+ *
+ * Created on October 1, 2015, 12:15 PM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+
+
+
+#endif	/* ARRAY_H */
+
---
+++ Class/Structured_Array/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on October 1, 2015, 12:11 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
