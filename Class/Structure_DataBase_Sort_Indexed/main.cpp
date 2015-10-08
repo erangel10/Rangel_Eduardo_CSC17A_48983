73a3ced583563c7cce7bc6f65dc4d8e3d9813eeb
erangel10 <rangeleduardo10@gmail.com>
2015-10-08 11:20:57 -0700

    Signed-off-by: erangel10 <rangeleduardo10@gmail.com>

--- Class/Structure_DataBase_Sort_Indexed/main.cpp
+++ Class/Structure_DataBase_Sort_Indexed/main.cpp
@@ -6,89 +6,92 @@
  * HMRK: convert structure to . operation and array to pointer notation
  */
 //System libraries
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
 #include <ctime>
 using namespace std;
-//User libraries
-#include <IndexedColumn.h>
+
+//User Library
+#include "IndexedColumn.h"
 
 //Global Constants
 
-//function Prototypes
+//Function Prototypes
 IndxCol *fill(int);
-void prntStr(IndxCol *, int);
-void prntAry(int *,int, int);
+void prntStr(IndxCol *,int);
+void prntAry(int *,int,int);
 void mrkSort(IndxCol *);
-//execution begins here
+
 int main(int argc, char** argv) {
+    //Call random number generate seed function
     srand(static_cast<unsigned int>(time(0)));
-    int n=100, col=10;
-    
+    //Allocate the memory for the structure
+    int n=100,col=10;
     IndxCol *str=fill(n);
+    //Print the structure
     cout<<"The original index array"<<endl;
-    prntAry(str-> array, str->size,col);
-    prntAry(str->indx, str->size,col);
-    prntStr(str, col);
-    //sort data
+    prntAry(str->indx,str->size,col);
+    cout<<"The original data array"<<endl;
+    prntAry(str->array,str->size,col);
+    cout<<"The original data array using the indexed array"<<endl;
+    prntStr(str,col);
+    //Sort the data using an index, hence data base sort
     mrkSort(str);
-    
+    //Print the structure
+    cout<<"The index array after sorting"<<endl;
+    prntAry(str->indx,str->size,col);
+    cout<<"The data array after sorting"<<endl;
+    prntAry(str->array,str->size,col);
+    cout<<"The data array using the indexed array after sorting"<<endl;
+    prntStr(str,col);
+    //Exit stage right
     return 0;
 }
 
-void mrkSort(IndxCol *a)
-{
-    for (int pos=0;pos<(*a).size;pos++)
-    {
-        for (int 1st=0;1st<a->size;1st++)
-        {
-            if(a->array[a->indx[pos]]<a->array[a->indx[1st]])
-            {
+void mrkSort(IndxCol *a){
+    //Pick a position to sort
+    for(int pos=0;pos<(*a).size-1;pos++){
+        //Sort a list by the position
+        for(int lst=pos+1;lst<a->size;lst++){
+            if(a->array[a->indx[pos]]>a->array[a->indx[lst]]){
                 int temp=a->indx[pos];
-                a->indx[pos]=a->indx[1st];
-                a->indx[1st]=temp;
+                a->indx[pos]=a->indx[lst];
+                a->indx[lst]=temp;
             }
         }
     }
-    
 }
 
-void prntAry(int *a, int size, int perLine)
-{
+void prntAry(int *a,int size,int perLine){
     cout<<endl;
-    for(int i=0;i<a->size;i++)
-    {
-        cout<<a->array[a->indx[i]]<<" "<<;
-        if (i%perLine==(perLine-1))<<endl;
+    for(int i=0;i<size;i++){
+        cout<<a[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
     }
     cout<<endl;
-    
 }
 
-void prntStr(IndxCol *a, int perLine)
-{
+void prntStr(IndxCol *a,int perLine){
     cout<<endl;
-    for(int i=0;i<a;i++)
-    {
-        cout<<a->array[a->indx[i]]<<" "<<;
-        if (i%perLine==(perLine-1))<<endl;
+    for(int i=0;i<a->size;i++){
+        cout<<a->array[a->indx[i]]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
     }
     cout<<endl;
-    
 }
 
-IndxCol *fill(int n)
-{
+IndxCol *fill(int n){
     //Allocate memory
     IndxCol *col=new IndxCol;
-    if (n<=1)n=2;
+    if(n<=1)n=2;
     col->size=n;
     col->array=new int[n];
     col->indx=new int[n];
-    for (int i=0;i<n;i++)
-    {
+    //Fill the array
+    for(int i=0;i<n;i++){
         col->indx[i]=i;
         col->array[i]=rand()%90+10;
     }
+    //Return the allocated and filled structure
+    return col;
 }
-
