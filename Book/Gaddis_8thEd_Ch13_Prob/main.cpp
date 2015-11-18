866ad07afbbc77c99b4474852bff7ad148a33865
erangel10 <rangeleduardo10@gmail.com>
2015-11-17 19:10:11 -0800

    Signed-off-by: erangel10 <rangeleduardo10@gmail.com>

---
+++ Book/Gaddis_8thEd_Ch13_Prob/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: EDUARDO
+ *
+ * Created on November 17, 2015, 6:09 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
